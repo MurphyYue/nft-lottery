baaf0aea5dac2c1239207e9c9c9741d71082e760
murphyyue <murphyyue@icloud.com>
2024-09-23 21:30:27 +0800

    Update abi address

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ import VGodABI from "@abis/VGodABI.json";
 import PaymentSplitterReferenceABI from "@abis/PaymentSplitterReferenceABI.json";
 
 
-export const VGodAddr = "0x346036103C2BBA394d919384De5aa16B426463E4";
+export const VGodAddr = "0x4ba4FE863D29f48924573Eb8E8c0674C2C205987";
 export const PaymentSplitterReferenceAddr = "0x40c97c73a96623fc6e7b9b90dc8aeb15f8b47dc1";
 
 
