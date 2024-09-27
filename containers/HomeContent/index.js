67a084efced368cd970d65d7029fb902bcd012cc
murphyyue <murphyyue@icloud.com>
2024-09-27 16:18:10 +0800

    update homepage writting text

--- containers/HomeContent/index.js
+++ containers/HomeContent/index.js
@@ -30,13 +30,14 @@ const Home = () => {
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                win-win
+                Win-Win
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                ERC721-C: Each minter gets a 3% royalty on the transaction.
+                ERC721-C: Each minter earns 3% royalty per transaction.
               </p>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Airdrop Meme tokens to creators, miners, traders.
+                Airdrop: Distribute Meme tokens to creators, miners, and
+                traders.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -46,10 +47,11 @@ const Home = () => {
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                Meme
+                Meme Token
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Create the meme token with the strongest consensus and the largest market cap.
+                Build the strongest consensus and achieve the largest market
+                cap.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -58,11 +60,12 @@ const Home = () => {
           </div>
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
-              {/* <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                Meme
-              </h3> */}
+              <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
+                Together
+              </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Together for the construction of Ethereum ecological construction to do more...
+                Let's enhance the Ethereum ecosystem for greater growth and
+                innovation.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
