2e62cb07f84f335aaeebbc338de032584681d3ef
murphyyue <murphyyue@icloud.com>
2024-10-21 21:51:51 +0800

    update texting

--- containers/HomeContent/index.js
+++ containers/HomeContent/index.js
@@ -6,14 +6,17 @@ import HomeBg4 from "@images/ether_4.png";
 
 const Home = () => {
   return (
-    <div className="px-4 sm:px-8 lg:px-16 overflow-y-auto" style={{height: 'calc(100vh - 152px)'}}>
+    <div
+      className="px-4 sm:px-8 lg:px-16 overflow-y-auto"
+      style={{ height: "calc(100vh - 152px)" }}
+    >
       <main>
         <SaleTime />
         <div>
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%]">
               <h3 className="text-2xl md:text-6xl font-semibold text-center md:text-left md:mt-12">
-                Consensuss
+                Consensuss:
               </h3>
               <p className="text-base md:text-4xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
                 Biger!
@@ -29,14 +32,13 @@ const Home = () => {
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                Win-Win
+                Win-Win:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 ERC721-C: Each minter earns 3% royalty per transaction.
               </p>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Airdrop: Distribute Meme tokens to creators, miners, and
-                traders.
+                Inviter: Get royalties and mint fees.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -46,11 +48,13 @@ const Home = () => {
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                Meme Token
+                Meme Token:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Build the strongest consensus and achieve the largest market
-                cap.
+                Build the strongest consensus and achieve the largest market cap.
+              </p>
+              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+                Distribute Meme tokens to miners, traders, and Inviter.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -60,11 +64,10 @@ const Home = () => {
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%] ml-0 md:ml-4">
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
-                Together
+                Together:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Let's enhance the Ethereum ecosystem for greater growth and
-                innovation.
+                Let's enhance the Ethereum ecosystem for greater growth and innovation.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
