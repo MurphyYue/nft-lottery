53bf635ecedc659c558c087681488b77527b1e57
murphyyue <murphyyue@icloud.com>
2024-11-05 17:25:48 +0800

    fix: display inviterAddress in MintModal input

--- pages/mint/MintModal.js
+++ pages/mint/MintModal.js
@@ -1,11 +1,14 @@
 import { Button, Modal, Input } from "@lidofinance/lido-ui";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
 
 function MintModal({ open, onClose, onMint }) {
   const inviterAddressUrl = useSelector((state) => state.user.inviterAddress);
   const [inviterAddress, setInviterAddress] = useState(inviterAddressUrl);
 
+  useEffect(() => {
+    setInviterAddress(inviterAddressUrl);
+  }, [inviterAddressUrl]);
   const handleInputChange = (e) => {
     setInviterAddress(e.target.value);
   };
