06f817d8cbdafddc38fb7e35680b076cc4b9d9c7
murphyyue <murphyyue@icloud.com>
2024-11-20 18:57:42 +0800

    update text content

--- containers/HomeContent/index.js
+++ containers/HomeContent/index.js
@@ -30,18 +30,23 @@ const Home = () => {
       style={{ height: "calc(100vh - 152px)" }}
     >
       <main>
-        <div className="w-full text-center text-4xl font-semibold py-4">{isPaused ? "Mint coming soon!" : null}</div>
+        <div className="w-full text-center text-4xl font-semibold py-4">
+          {isPaused ? "Mint coming soon!" : null}
+        </div>
         <div>
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%]">
               <h3 className="text-2xl md:text-6xl font-semibold text-center md:text-left md:mt-12">
-                Consensuss:
+                Strongest Consensus:
               </h3>
               <p className="text-base md:text-4xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
-                Biger!
+                Bigger! Stronger!
               </p>
               <p className="text-base md:text-4xl md:ml-16 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
-                Stronger!
+                Let's talk about ETH market value!
+              </p>
+              <p className="text-base md:text-4xl md:ml-16 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
+                Check out Vitalik Buterin's AI Immortal Virtual Human!
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -54,10 +59,16 @@ const Home = () => {
                 Win-Win:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                ERC721-C: Each minter earns 3% royalty per transaction.
+                ERC721-C: Minters earn 3% royalties on every transaction.
               </p>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Inviter: Get royalties and mint fees.
+                Get a random Token ID.
+              </p>
+              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+                One token per address.
+              </p>
+              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+                Creators, Minters, Inviters, and Traders all share equal rights.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -70,10 +81,11 @@ const Home = () => {
                 Meme Token:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Build the strongest consensus and achieve the largest market cap.
+                Build the strongest consensus and reach the biggest market cap.
               </p>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Distribute Meme tokens to miners, traders, and Inviter.
+                Distribute Meme tokens to creators, minters, inviters, and
+                traders.
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
@@ -86,7 +98,8 @@ const Home = () => {
                 Together:
               </h3>
               <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
-                Let's enhance the Ethereum ecosystem for greater growth and innovation.
+                Let's supercharge the Ethereum ecosystem for even more growth
+                and innovation!
               </p>
             </div>
             <div className="rounded-3xl w-full mb-4 sm:mb-10 overflow-hidden relative">
