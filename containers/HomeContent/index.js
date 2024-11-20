3bcd1b587a21120fe2786bb9d0a471c763009987
murphyyue <murphyyue@icloud.com>
2024-11-20 22:49:51 +0800

    update home style

--- containers/HomeContent/index.js
+++ containers/HomeContent/index.js
@@ -36,16 +36,16 @@ const Home = () => {
         <div>
           <div className="flex flex-col md:flex-row md:even:flex-row-reverse">
             <div className="w-full md:max-w-[30%]">
-              <h3 className="text-2xl md:text-6xl font-semibold text-center md:text-left md:mt-12">
+              <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
                 Strongest Consensus:
               </h3>
-              <p className="text-base md:text-4xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
                 Bigger! Stronger!
               </p>
-              <p className="text-base md:text-4xl md:ml-16 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
                 Let's talk about ETH market value!
               </p>
-              <p className="text-base md:text-4xl md:ml-16 md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-center md:text-left">
                 Check out Vitalik Buterin's AI Immortal Virtual Human!
               </p>
             </div>
@@ -58,16 +58,16 @@ const Home = () => {
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
                 Win-Win:
               </h3>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 ERC721-C: Minters earn 3% royalties on every transaction.
               </p>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 Get a random Token ID.
               </p>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 One token per address.
               </p>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 Creators, Minters, Inviters, and Traders all share equal rights.
               </p>
             </div>
@@ -80,10 +80,10 @@ const Home = () => {
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
                 Meme Token:
               </h3>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 Build the strongest consensus and reach the biggest market cap.
               </p>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 Distribute Meme tokens to creators, minters, inviters, and
                 traders.
               </p>
@@ -97,7 +97,7 @@ const Home = () => {
               <h3 className="text-2xl md:text-4xl font-semibold text-center md:text-left md:mt-12">
                 Together:
               </h3>
-              <p className="text-base md:text-2xl md:ml-8 md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
+              <p className="text-base md:text-2xl md:mt-10 mb-2 leading-relaxed text-gray-700 text-left">
                 Let's supercharge the Ethereum ecosystem for even more growth
                 and innovation!
               </p>
