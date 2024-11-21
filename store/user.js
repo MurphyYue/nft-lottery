5e523f1ef2b50fd4b2f751c4376e9417521cda92
murphyyue <murphyyue@icloud.com>
2024-11-21 19:50:01 +0800

    switch network to polygon

--- store/user.js
+++ store/user.js
@@ -15,7 +15,7 @@ export const slice = createSlice({
       web3_address: '', // 钱包地址
       followers_count: '' // 粉丝数
     },
-    selectedNetwork: ETH_TYPE, // 默认网络
+    selectedNetwork: POLYGON_TYPE, // 默认网络
     inviterAddress: '',
     minted: false,
   },
